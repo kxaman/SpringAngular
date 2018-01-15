94dcc496eb002810657f8b1b577d95efe4c2ceb2
fibacache <fibacache@entel.cl>
2018-01-15 12:08:43 -0300

    First Commit

---
+++ src/main/java/com/ejemplo2/Ejemplo2Application.java
@@ -0,0 +1,12 @@
+package com.ejemplo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo2Application.class, args);
+	}
+}
